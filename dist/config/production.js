fffe5cd1bb04f053b39d010f7d4f6d3cb95e41e4
bip565 <biplab.mondal@appsbee.com>
2020-12-31 11:30:36 +0530

    updated

---
+++ dist/config/production.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var config = {
+  //for production
+  foodjetsUrl: 'https://foodjets.com',
+  api_end: '/mbp-service/',
+  GTM_code: 'GTM-THNGPH6',
+  GSTM_code: 'UA-65554301-3',
+  sentry_dsn: 'https://0931dde1b233432b95b706ef105cb284@sentry.io/1546472',
+  firebase: {
+    apiKey: "AIzaSyBxv2NuRYUB9SWlHKVNl-4dAoHtaEyHAKc",
+    authDomain: "foodjets-4bc9f.firebaseapp.com",
+    databaseURL: "https://foodjets-4bc9f.firebaseio.com",
+    projectId: "foodjets-4bc9f",
+    storageBucket: "foodjets-4bc9f.appspot.com",
+    messagingSenderId: "644501783160",
+    appId: "1:644501783160:web:e4d8c6cf0cf83c82"
+  },
+  paypal: {
+    mode: 'production'
+  }
+};
